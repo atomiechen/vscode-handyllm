62cbfebaf9103e8bab2e9e8dc140476262b2a18f
Atomie CHEN <atomic_cwh@163.com>
2024-05-04 22:25:07 +0800

    feat: new file uses a template

--- src/extension.ts
+++ src/extension.ts
@@ -40,7 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposableCreateHprompt = vscode.commands.registerCommand('handyllm.createHprompt', function () {
 		vscode.workspace.openTextDocument({
-			content: '',
+			content: '---\n# add YAML frontmatter data here\n\n---\n\n$system$\nYou are a helpful assistant.\n\n$user$\nPlace you instructions here.\n\n',
 			language: 'hprompt' // set the language mode to hprompt
 		}).then(document => {
 			vscode.window.showTextDocument(document);
