86d0ef0f7658bec4c67a5fcd0de1e182abd35d79
Atomie CHEN <atomic_cwh@163.com>
2024-05-21 00:23:45 +0800

    refactor: move command registrations to separate files

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { registerCommandCreate } from './cmd_create';
+import { registerCommandRun } from './cmd_run';
 
-function getOrCreateTerminal(name: string): vscode.Terminal {
-	// check if the terminal already exists
-	const existingTerminal = vscode.window.terminals.find(terminal => terminal.name === name);
-	if (existingTerminal) {
-		return existingTerminal;
-	} else {
-		return vscode.window.createTerminal(name);
-	}
-}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,47 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('"handyllm" extension is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposableRunHprompt = vscode.commands.registerCommand('handyllm.runHprompt', (uri: vscode.Uri) => {
-		let filePath = undefined;
-		if (uri) {
-			filePath = uri.fsPath;
-		} else {
-			const activeEditor = vscode.window.activeTextEditor;
-			if (activeEditor) {
-				// get the file path of the currently active file
-				filePath = activeEditor.document.uri.fsPath;
-			}
-		}
-		if (filePath) {
-			// get handyllm command name from the settings
-			let handyllmCommand = vscode.workspace.getConfiguration().get('handyllm.commandName', 'handyllm').trim();
-			if (handyllmCommand === '') {
-				handyllmCommand = 'handyllm';
-			}
-			// get or create a terminal with the name "hprompt"
-			const terminal = getOrCreateTerminal("hprompt");
-			terminal.show(true);
-			// run the hprompt command in the terminal
-			terminal.sendText(`${handyllmCommand} hprompt ${filePath}`);
-		} else {
-			// Display a message box to the user
-			vscode.window.showErrorMessage('No active editor found!');
-		}
-	});
-
-	let disposableCreateHprompt = vscode.commands.registerCommand('handyllm.createHprompt', function () {
-		vscode.workspace.openTextDocument({
-			content: '---\n# add YAML frontmatter data here\n\n---\n\n$system$\nYou are a helpful assistant.\n\n$user$\nPlace you instructions here.\n\n',
-			language: 'hprompt' // set the language mode to hprompt
-		}).then(document => {
-			vscode.window.showTextDocument(document);
-		});
-	});
-
-	context.subscriptions.push(disposableRunHprompt, disposableCreateHprompt);
+	registerCommandCreate(context);
+	registerCommandRun(context);
 }
 
 // This method is called when your extension is deactivated
